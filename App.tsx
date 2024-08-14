5e4fa388a1aebd0a30fe94deac8d96dee730e80b
fmacedosantos <felipemacedo986@gmail.com>
2024-08-14 12:16:43 -0300

    Instanciando o useState

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.big}>0</Text>
+
+      <View style={styles.inline}>
+        <Button title='REMOVER'></Button>
+        <Button title='ADICIONAR'></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  big: {
+    fontSize: 40
+  },
+  inline: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '50%'
+  }
 });
